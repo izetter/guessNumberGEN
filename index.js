53474c299913ede6c3814fb54faa7fc0f0e4c475
izetter <59151193+izetter@users.noreply.github.com>
2023-08-16 21:19:36 -0600

    Clarification on comments

--- index.js
+++ index.js
@@ -14,15 +14,22 @@ function guessNumber(min, max) {
 			guess = Math.floor((currentMax - currentMin) / 2 + currentMin);
 		} else if (confirm(`¿Tu número es ${guess}?`)) {
 			keepGuessing = false;
-			break;
+			break;  // V
 		} else {
 			currentMax = guess - 1;
 			guess = Math.floor((currentMax - currentMin) / 2 + currentMin);
 		}
-		if (currentMin === currentMax) keepGuessing = false;
 		results.push({guess, currentMin, currentMax});	// V
 	}
 	console.table(results);	// V
 	alert(`Tu número es ${guess}, excelente decisión.`)
 	return guess;
 }
+
+
+
+/* 
+The ` break; ` on line 17 is for visualization because it prevents
+line 22, which is also for visualization, from running when the number has been guessed,
+and thus, avoids an extra, duplicate line in the "report" table in console.
+*/
